cfa5e1380b357b1f217bf89ed1d6c8f1a4641d90
DESKTOP-MGFJ5MI\wbc <1147072143@qq.com>
2018-05-23 23:06:33 +0800

    提交注释

--- src/main/java/tools/MyTools.java
+++ src/main/java/tools/MyTools.java
@@ -4,6 +4,16 @@ import org.apache.log4j.Logger;
 
 public class MyTools {
 	
+	/**
+	 * @描述：重复做某件事，直到满足条件为止
+	 * @作者：wbc
+	 * @param event
+	 * @param condition
+	 * @param count
+	 * @param content
+	 * @return
+	 * @throws Exception
+	 */
 	public static Boolean tryToDoSomethingByConditionAndRetryCount(HandleBridge event, HandleBridge condition, int count, String content) throws Exception {
 		Logger logger = Logger.getRootLogger();
 		while (!condition.handleSomething()) {
